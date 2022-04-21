6d42a78aa44f0a4ff822b95c2e47cc41eee1351e
sirSantana23 <salazarmiguelito23gmail.com>
2022-04-21 16:04:38 -0500

    ModeloCardPost

--- components/Posts/Post/Post.js
+++ components/Posts/Post/Post.js
@@ -1,31 +1,155 @@
 import Link from "next/link";
+import {
+  Card,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Button,
+  Typography,
+  Divider,
+  IconButton,
+  Avatar,
+  CardHeader,
+} from "@material-ui/core";
+import Share from "@material-ui/icons/Share";
+import Delete from "@material-ui/icons/Delete";
+import Favorite from "@material-ui/icons/Favorite";
+import { FavoriteBorder } from "@material-ui/icons/";
+import { useEffect, useState } from "react";
+import moment from "moment";
+import useStyles from "./styles";
+import { useRouter } from "next/router";
+import logoChevrolet from '../../../public/images/LOGO_CHEVROLET.png'
 
-export default function PostCo({ Post, handleDelete}) {
 
+export default function PostCo({ Post }) {
+  const classes = useStyles();
+  const { marca, repuesto, selectedFile, date, likes } = Post;
+  const [user, setUser] = useState(null);
+  const [imagen, setImagen] = useState(false);
+  // const dispatch = useDispatch();
+  const router = useRouter();
+  const idCreator = Post?.creator[0];
+  const nombreCreador = Post?.nombreCreador.toString();
+
+  const handleFavorite = (e) => {
+    // dispatch(favoritePost(Post._id));
+  };
+  const handleDelete = () => {
+    // dispatch(deletePost(Post._id));
+    router.push("/postEliminado");
+  };
+  const handleCotizar = (e) => {
+    if (user?.result) {
+      router.push(`/posts/${Post._id}`);
+    } else {
+      router.push("/auth");
+    }
+  };
+
+  // const Likes = () => {
+  //   if (Post.favorites.length > 0) {
+  //     return Post.favorites.find(
+  //       (favorite) => favorite === user?.result?._id
+  //     ) ? (
+  //       <>
+  //         <FavoriteIcon fontSize="small" />
+  //         &nbsp;
+  //         {Post.favorites.length > 2
+  //           ? `Tu y  ${Post.favorites.length - 1} mas`
+  //           : `${Post.favorites.length} ${Post.favorites.length > 1 ? "" : ""}`}
+  //       </>
+  //     ) : (
+  //       <>
+  //         <FavoriteBorderIcon fontSize="small" />
+  //         &nbsp;{Post.favorites.length} {Post.favorites.length === 1 ? "" : ""}
+  //       </>
+  //     );
+  //   }
+
+  //   return (
+  //     <>
+  //       <FavoriteBorderIcon fontSize="small" />
+  //       &nbsp;
+  //     </>
+  //   );
+  // };
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, []);
   return (
     <>
-      <h2>
-        <b>Post</b>
-        {Post._id}
-      </h2>
-      <h3>
-        <b>Marca:</b> {Post.marca}
-      </h3>
-      <h4>
-        <b>Repuesto:</b> {Post.repuesto}
-      </h4>
-      <h6>
-        <b>Fecha:</b> {Post.date}
-      </h6>
-      <h4>
-        <b>Cliente:</b> {Post.nombreCreador[0]}
-      </h4>
-      <Link href={"/posts"}>
-        <a>
-          <button>Regresar</button>
-        </a>
-      </Link>
-      <button onClick={handleDelete}>Eliminar </button>
+      <Card sx={{ maxWidth: 345 }} className={classes.card} elevation={2}>
+        <CardHeader
+          className={classes.header}
+          avatar={
+            <Avatar src={'/images/LOGO_CHEVROLET.png'} className={classes.purple} alt={Post?.marca}>
+              {nombreCreador?.substr(0, 1)}
+            </Avatar>
+          }
+          title={marca}
+          subheader={moment(date).fromNow()}
+        />
+        <Divider></Divider>
+
+        <CardContent>
+          <Typography variant="body2">{repuesto}</Typography>
+          {selectedFile ? (
+            <Button
+              className={classes.button}
+              color={imagen ? "secondary" : "primary"}
+              variant="contained"
+              onClick={() => (imagen ? setImagen(false) : setImagen(true))}
+            >
+              {imagen ? "X" : "Revisa la foto"}
+            </Button>
+          ) : null}
+          {imagen ? (
+            <>
+              <CardMedia className={classes.media} image={Post.selectedFile} />
+            </>
+          ) : null}
+        </CardContent>
+        <CardActions disableSpacing>
+          <Button
+            size="small"
+            disabled={!user?.result}
+            onClick={handleFavorite}
+          >
+            {/* <Likes /> */}
+          </Button>
+          <IconButton aria-label="share">
+            <Share />
+          </IconButton>
+
+          {user?.result?._id === idCreator && (
+            <Button size="small" onClick={handleDelete}>
+              <Delete fontSize="small" />
+            </Button>
+          )}
+        </CardActions>
+
+        {user?.result._id === idCreator ? (
+          <Button
+            color="primary"
+            variant="contained"
+            className={classes.cotizar}
+            fullWidth
+            onClick={handleCotizar}
+          >
+            Mira las Cotizaciones
+          </Button>
+        ) : (
+          <Button
+            color="secondary"
+            variant="contained"
+            fullWidth
+            onClick={handleCotizar}
+          >
+            Cotiza Ya!
+          </Button>
+        )}
+      </Card>
     </>
   );
 }
---
+++ components/Posts/Post/styles.js
@@ -0,0 +1,107 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
+export default makeStyles((theme)=>({
+  media: {
+    height: 0,
+    paddingTop: '86.25%',
+    backgroundColor: 'rgb(0, 0, 0, )',
+    backgroundBlendMode: 'darken',
+    justifyContent: 'space-between'
+  },
+  border: {
+    border: 'solid',
+  },
+  fullHeightCard: {
+    height: '90%',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    borderRadius: '15px',
+    height: '100%',
+    padding: '0',
+    position: 'relative',
+  },
+  card1: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadiusTop: '15px',
+    height: '5px',
+    columns: "5, 320px",
+    fontFamily:'-apple-system',
+    backgroundColor: "#1b333d",
+    color: 'white',
+    white: '100%'
+  },
+  purple: {
+    color: 'black',
+    height:'80px',
+    width:'80px',
+    padding:0,
+    margin:0
+
+  },
+  typography1:{
+    height: '50px',
+    padding: '0',
+    margin:'0'
+
+  },
+  overlay: {
+    color: 'black',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '50px',
+  },
+  overlay2: {
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
+  },
+  grid: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '20px',
+  },
+  title: {
+    padding: '0 16px',
+  },
+  cardActions: {
+    padding: '0 16px 8px 16px',
+    display: 'inline-flex',
+    justifyContent: 'space-between',
+
+  },
+  header:{
+    backgroundColor:'gray',
+    color:'#f1f1f1',
+    fontFamily:"-apple-system, Segoe UI",
+    fontSize:'35px',
+    padding:"0px"
+  },
+  typography:{
+    padding: '0',
+    margin:'0',
+    fontSize: '19px',
+
+  },
+  button:{
+    alignItems:'bottom',
+    width:'fit-content',
+    padding: '0 10px',
+    marginLeft:'0',
+    marginTop:'15px'
+  },
+  cotizar:{
+    backgroundColor: "#2f7c9c"
+  },
+  
+}));

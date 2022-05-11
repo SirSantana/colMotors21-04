937ba5a42701c3648933d217baa82096c0f50cb2
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-05-10 21:19:26 -0500

    x11

--- pages/home/index.js
+++ pages/home/index.js
@@ -21,7 +21,7 @@ export default function Home({Postss}) {
   const createPosts = async (postData) => {
     try {
       const res = await fetch(
-       "https://col-motors21-04.vercel.app/api/posts"
+       "https://col-motors.vercel/api/posts"
       , {
         method: "POST",
         headers: {"Content-type": "application/json"},

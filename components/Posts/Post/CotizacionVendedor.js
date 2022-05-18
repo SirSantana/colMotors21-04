2721d13b36280023411be7727168fd68ded2076f
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-05-18 09:25:42 -0500

    pt6

--- components/Posts/Post/CotizacionVendedor.js
+++ components/Posts/Post/CotizacionVendedor.js
@@ -16,13 +16,14 @@ import PostContent from "./PostContent";
 import useStyles from "./styles";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { LOGOUT } from "../../../reducers/type";
 
 export default function CotizacionVendedor({user,OnePost,cotizacionCreada}) {
   const dispatch = useDispatch();
   const classes = useStyles();
   const { cotizacion } = useSelector((state) => state.cotizaciones);
+  // let cotizacion;
   const router = useRouter()
-  console.log(cotizacion);
     // let name = cotizacion?.nombreVendedor[0].toString();
   // let indice;
   // let two;
@@ -34,151 +35,154 @@ export default function CotizacionVendedor({user,OnePost,cotizacionCreada}) {
   //   let twoLetters = name[0] + secondLetter
   //    two = twoLetters.replace(/ /g, "")
   // }
-
+  setTimeout(()=>{
+    if(!cotizacion){
+      dispatch({ type: LOGOUT });
+    return router.push('/auth')
+  }
+  },3000)
 
   useEffect(() => {
     dispatch(getCotizacion(cotizacionCreada));
   }, [dispatch]);
   return (
     <>
-      {cotizacion &&
       <div className={classes.card}>
-      <div className={classes.header1}>
-        <Typography gutterBottom className={classes.typo}>
-          {user?.result._id === OnePost?.creator
-          ? <b>Cotización Vendedor</b>
-        : <b>Tu Cotización</b>}
-        </Typography>
-      </div>
-      <Card sx={{ width: "345px" }} className={classes.card1} elevation={2}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <CardHeader
-            className={classes.header}
-            avatar={
-              <Avatar
-              className={classes.purple2}
-                alt={cotizacion?.nombreVendedor[0]}
-              >
-                {/* {two} */}
-              </Avatar>
-            }
-            title={cotizacion?.nombreVendedor[0]}
-            classes={{ subheader: classes.subheader, title: classes.title }}
-            subheaderTypographyProps={{ variant: "body2" }}
-            subheader="Vendedor"
-          />
-          <section
-            style={{
-              marginRight: "10px",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Menu />
-          </section>
+        <div className={classes.header1}>
+          <Typography gutterBottom className={classes.typo}>
+            {user?.result._id === OnePost?.creator
+            ? <b>Cotización Vendedor</b>
+          : <b>Tu Cotización</b>}
+          </Typography>
         </div>
-
-        <Divider></Divider>
-
-        <CardContent
-          style={{
-            width: "95%",
-            display: "flex",
-            flexDirection: "column",
-            gap: "10px",
-            padding: "8px",
-            paddingLeft: "4px",
-          }}
-        >
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                marginBottom: "8px",
-              }}
-            >
-              <AttachMoney fontSize="large" />
-              <Typography className={classes.title}>
-                {cotizacion?.precio}
-              </Typography>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                marginLeft: "5px",
-              }}
-            >
-              <Build fontSize="medium" />
-              <Typography
-                style={{ marginLeft: "5px" }}
-                className={classes.typography1}
-              >
-                {cotizacion?.repuestos}
-              </Typography>
-            </div>
-          </div>
-        </CardContent>
-
-        <CardActions
-          style={{ width: "90%", paddingLeft: "8px", paddingBottom: "0" }}
-        >
+        <Card sx={{ width: "345px" }} className={classes.card1} elevation={2}>
           <div
             style={{
               display: "flex",
-              flexDirecction: "row",
+              flexDirection: "row",
+              justifyContent: "space-between",
               alignItems: "center",
             }}
           >
-            <Place
-              style={{ color: "#1b333d", width: "30px", height: "30px" }}
+            <CardHeader
+              className={classes.header}
+              avatar={
+                <Avatar
+                className={classes.purple2}
+                  alt={cotizacion?.nombreVendedor[0]}
+                >
+                  {/* {two} */}
+                </Avatar>
+              }
+              title={cotizacion?.nombreVendedor[0]}
+              classes={{ subheader: classes.subheader, title: classes.title }}
+              subheaderTypographyProps={{ variant: "body2" }}
+              subheader="Vendedor"
             />
-            <Typography style={{ marginLeft: "5px" }} variant="body1">
-              {cotizacion?.creator.pais}
-            </Typography>
-          </div>
-        </CardActions>
-        {user?.result?._id === OnePost.creator ? (
-          <>
-            <Button
-              color="primary"
-              variant="contained"
-              fullWidth
-              className={classes.cotizar}
+            <section
+              style={{
+                marginRight: "10px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
             >
-              Ver Pefil Vendedor
-            </Button>
+              <Menu />
+            </section>
+          </div>
+
+          <Divider></Divider>
+
+          <CardContent
+            style={{
+              width: "95%",
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              padding: "8px",
+              paddingLeft: "4px",
+            }}
+          >
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginBottom: "8px",
+                }}
+              >
+                <AttachMoney fontSize="large" />
+                <Typography className={classes.title}>
+                  {cotizacion?.precio}
+                </Typography>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  marginLeft: "5px",
+                }}
+              >
+                <Build fontSize="medium" />
+                <Typography
+                  style={{ marginLeft: "5px" }}
+                  className={classes.typography1}
+                >
+                  {cotizacion?.repuestos}
+                </Typography>
+              </div>
+            </div>
+          </CardContent>
+
+          <CardActions
+            style={{ width: "90%", paddingLeft: "8px", paddingBottom: "0" }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirecction: "row",
+                alignItems: "center",
+              }}
+            >
+              <Place
+                style={{ color: "#1b333d", width: "30px", height: "30px" }}
+              />
+              <Typography style={{ marginLeft: "5px" }} variant="body1">
+                {cotizacion?.creator.pais}
+              </Typography>
+            </div>
+          </CardActions>
+          {user?.result?._id === OnePost.creator ? (
+            <>
+              <Button
+                color="primary"
+                variant="contained"
+                fullWidth
+                className={classes.cotizar}
+              >
+                Ver Pefil Vendedor
+              </Button>
+              <Button
+                color="secondary"
+                variant="contained"
+                fullWidth
+                className={classes.cotizar}
+              >
+                Enviale un Mensaje
+              </Button>
+            </>
+          ) : (
             <Button
               color="secondary"
               variant="contained"
               fullWidth
               className={classes.cotizar}
             >
-              Enviale un Mensaje
+              Eliminar cotizacion
             </Button>
-          </>
-        ) : (
-          <Button
-            color="secondary"
-            variant="contained"
-            fullWidth
-            className={classes.cotizar}
-          >
-            Eliminar cotizacion
-          </Button>
-        )}
-      </Card>
-    </div>
-      }
+          )}
+        </Card>
+      </div>
     </>
   );
 }

49185370ff80aa19a0e2c48536728740c99d0041
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-05-19 09:41:26 -0500

    x4

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -20,23 +20,23 @@ export default function Prueba({Postss}) {
   );
 }
 
-export async function getServerSideProps() {
-  try {
-    await DBConnect();
-    const res = await postModel.find({});
-    const Postss = res.map((el) => {
-      const Post = el.toObject();
+// export async function getServerSideProps() {
+//   try {
+//     await DBConnect();
+//     const res = await postModel.find();
+//     const Postss = res.map((el) => {
+//       const Post = el.toObject();
 
-      Post._id = Post._id.toString();
-      Post.creator = Post.creator.toString();
-      Post.cotizaciones = Post.cotizaciones.toString();
-      Post.date = Post.date.toString();
-      return Post;
-    });
-    return {
-      props: { Postss },
-    };
-  } catch (error) {
-    console.log(error);
-  }
-}
+//       Post._id = Post._id.toString();
+//       Post.creator = Post.creator.toString();
+//       Post.cotizaciones = Post.cotizaciones.toString();
+//       Post.date = Post.date.toString();
+//       return Post;
+//     });
+//     return {
+//       props: { Postss },
+//     };
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }

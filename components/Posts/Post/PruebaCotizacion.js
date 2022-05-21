715010d748d93d6d6ef713ec856d48b55f1bf8a7
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-05-21 18:34:20 -0500

    x9

--- components/Posts/Post/PruebaCotizacion.js
+++ components/Posts/Post/PruebaCotizacion.js
@@ -4,8 +4,16 @@ import { useDispatch, useSelector } from 'react-redux'
 import Prueba from './Pruebas'
 import {useRouter} from 'next/router'
 import { getCotizacion, getCotizaciones } from '../../../reducers/Actions/cotizacionesActions'
-export default function PruebaCotizacion({cotizacionesVarias, cotizacion, cotizaciones}){
+export default function PruebaCotizacion({otroId}){
     
+    const {cotizacion, cotizaciones} = useSelector(state=> state.cotizaciones)
+
+    let cotizacion1 = [];
+    if(cotizaciones && otroId?.length >1){
+        cotizacion1.push(cotizaciones?.filter((ele) => ele._id === otroId[0].toString()))
+        cotizacion1.unshift(cotizaciones?.filter((ele) => ele._id === otroId[1].toString()))
+    }
+    const cotizacionesVarias = cotizacion1.flat()
     console.log('cotizacion1', cotizacionesVarias);
     console.log('renderizadoFuera');
  
--- pages/posts/cotizaciones/[id].js
+++ pages/posts/cotizaciones/[id].js
@@ -9,33 +9,22 @@ import { getCotizacion, getCotizaciones } from '../../../reducers/Actions/cotiza
 export default function Prubea(){
   const router = useRouter()
   const { id } = router.query
-  const {cotizacion, cotizaciones} = useSelector(state=> state.cotizaciones)
   
   const otroId = id?.split(",")
   const dispatch = useDispatch()
   
-  let cotizacion1 = [];
-  if(cotizaciones && otroId?.length >1){
-      cotizacion1.push(cotizaciones?.filter((ele) => ele._id === otroId[0].toString()))
-      cotizacion1.unshift(cotizaciones?.filter((ele) => ele._id === otroId[1].toString()))
-  }
-  const cotizacionesVarias = cotizacion1.flat()
+
 
 
   useEffect(()=>{
-    if(otroId?.length >1){
-      console.log('cotizaciones');
     dispatch(getCotizaciones())
-    }else if(otroId?.length <=1){
-      console.log('cotizacion');
-        dispatch(getCotizacion(otroId[0], router))
-    }
+    dispatch(getCotizacion(otroId[0], router))
 },[router, dispatch])
 
     return(
         <>
         <Layout title={"Cotizacion | colMotors"}>
-        {cotizacionesVarias&& <PruebaCotizacion cotizacionesVarias={cotizacionesVarias} cotizacion={cotizacion} cotizaciones={cotizaciones}/>  }      
+        <PruebaCotizacion otroId={otroId} />     
         {/* {cotizacionCreada && id !== undefined ? (
           <div className={classes.cotizarr}>
             <CotizacionVendedor user={user} OnePost={OnePost} el={cotizacionCreada}/>

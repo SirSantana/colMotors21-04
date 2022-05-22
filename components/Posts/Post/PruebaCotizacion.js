9856ef6b5e16c35fe8d955094f2842430ef35569
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-05-21 21:47:34 -0500

    x17

--- components/Posts/Post/PruebaCotizacion.js
+++ components/Posts/Post/PruebaCotizacion.js
@@ -5,31 +5,18 @@ import Prueba from './Pruebas'
 import {useRouter} from 'next/router'
 import { getCotizacion, getCotizaciones } from '../../../reducers/Actions/cotizacionesActions'
 import PostCo from './Post'
-export default function PruebaCotizacion({Post, otroId}){
-    const router = useRouter()
-    const dispatch = useDispatch()
-    const {cotizacion, cotizaciones} = useSelector(state=> state.cotizaciones)
-    
-    console.log("cotizacion",cotizacion);
-    console.log("cotizaciones",cotizaciones);
+export default function PruebaCotizacion({Post, otroId, cotizacion1}){
+  const { cotizacion, cotizaciones } = useSelector((state) => state.cotizaciones);
 
-    let cotizacion1 = [];
-    if(cotizaciones && otroId?.length >1){
-        cotizacion1.push(cotizaciones?.filter((ele) => ele._id === otroId[0].toString()))
-        cotizacion1.unshift(cotizaciones?.filter((ele) => ele._id === otroId[1].toString()))
-    }
+    const router = useRouter()
+    
+
+    
     const cotizacionesVarias = cotizacion1?.flat()
     
-    console.log("cotizaciones", cotizaciones);
-    console.log("cotizacion", cotizacion);
     console.log(cotizacionesVarias);
 
-    useEffect(()=>{
-        dispatch(getCotizaciones())
-        // if(otroId){
-        //     dispatch(getCotizacion(otroId[0]))
-        // }
-      },[router, dispatch])
+  
 
     return(
         <>
--- pages/posts/cotizaciones/[id].js
+++ pages/posts/cotizaciones/[id].js
@@ -1,38 +1,62 @@
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import Layout from '../../../components/Layout/Layout'
-import PruebaCotizacion from '../../../components/Posts/Post/PruebaCotizacion'
-import DBConnect from '../../../libs/dbConnect'
-import postModel from '../../../models/postModel'
+import Layout from "../../../components/Layout/Layout";
+import PruebaCotizacion from "../../../components/Posts/Post/PruebaCotizacion";
+import DBConnect from "../../../libs/dbConnect";
+import postModel from "../../../models/postModel";
 
-import { getCotizacion, getCotizaciones } from '../../../reducers/Actions/cotizacionesActions'
+import {
+  getCotizacion,
+  getCotizaciones,
+} from "../../../reducers/Actions/cotizacionesActions";
 
-export default function Prubea({Post}){
-  const router = useRouter()
-  const { id } = router.query
+export default function Prubea({ Post }) {
+  const router = useRouter();
+  const { id } = router.query;
+  const dispatch = useDispatch()
+  console.log(id);
+
+  const otroId = id?.split(",");
+  let cotizacion1 = [];
+  const { cotizacion, cotizaciones } = useSelector((state) => state.cotizaciones);
+
+  if (cotizaciones && otroId?.length > 1) {
+    cotizacion1.push(
+      cotizaciones?.filter((ele) => ele._id === otroId[0].toString())
+    );
+    cotizacion1.unshift(
+      cotizaciones?.filter((ele) => ele._id === otroId[1].toString())
+    );
+  }
   
-  const otroId = id?.split(",")
-    return(
-        <>
-        <Layout title={"Cotizacion | colMotors"}>
-        <PruebaCotizacion Post={Post} otroId={otroId}/>    
+  console.log("cotizaciones", cotizaciones);
+  console.log("cotizacion", cotizacion);
+  useEffect(() => {
+    dispatch(getCotizaciones());
+    if(otroId){
+        dispatch(getCotizacion(otroId[0]))
+    }
+  }, [router, dispatch]);
+  return (
+    <>
+      <Layout title={"Cotizacion | colMotors"}>
+        <PruebaCotizacion Post={Post} otroId={otroId} cotizacion1={cotizacion1}/>
         {/* {cotizacionCreada && id !== undefined ? (
           <div className={classes.cotizarr}>
             <CotizacionVendedor user={user} OnePost={OnePost} el={cotizacionCreada}/>
           </div>
         ) : null} */}
-          {/* <Button variant="contained" color='secondary' onClick={()=> router.push("/home")}>Regresa</Button> */}
+        {/* <Button variant="contained" color='secondary' onClick={()=> router.push("/home")}>Regresa</Button> */}
         {/* {user?.result._id === OnePost.creator && id !== undefined ?
         <div>
             <CotizacionVendedor user={user} OnePost={OnePost} arrayCotizaciones={arrayCotizaciones}/>
         </div>
         :null} */}
-
-        </Layout>
-        </>
-    )
+      </Layout>
+    </>
+  );
 }
 export async function getServerSideProps({ params, query }) {
   try {

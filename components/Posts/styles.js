38342bdb6dc10c05b0d05c5fb4e014589374d8d6
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-06-15 11:08:02 -0500

    AcomodandoVistaVendedor1

--- components/Posts/styles.js
+++ components/Posts/styles.js
@@ -10,7 +10,7 @@ export default makeStyles((theme) => ({
     // justifyContent: 'flex-end',
     maxWidth: '1000px',
     [theme.breakpoints.down('xs')]: {
-      maxWidth:'100vw',
+      maxWidth:'90vw',
       alignItems:'center',
 
     }},

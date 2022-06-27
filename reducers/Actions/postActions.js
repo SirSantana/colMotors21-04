90daab48b8a974f6221e9d4e224be163143d69be
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-06-27 10:18:07 -0500

    x9

--- reducers/Actions/postActions.js
+++ reducers/Actions/postActions.js
@@ -40,11 +40,11 @@ console.log(router);
   try {
 
     const { data } = await api.createPost(post);
+    
+    dispatch({ type: CREATE_POST, payload: data });
     if(data){
       router.push(`/posts/${data.newPost._id}`)
     }
-    dispatch({ type: CREATE_POST, payload: data });
-    
   } catch (error) {
     console.log(error.message);
   }

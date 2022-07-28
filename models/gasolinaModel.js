a183214c6f4d29e3eae2cc385e4e26e48b49cbfd
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-07-28 14:29:56 -0500

    x4

--- models/gasolinaModel.js
+++ models/gasolinaModel.js
@@ -7,8 +7,6 @@ const gasolinaSchema = new mongoose.Schema({
     kilometraje:{type:String},
     dineroGastado:{type:String},
     fecha:{type:Date, default:Date.now},
-    fuelInicio:{type:Number},
-    fuelFinal:{type:Number},
     owner:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     vehiculo:[{type: mongoose.Schema.Types.ObjectId, ref: 'Vehiculo'}]
 
